56f392246efb201a888ca6b0374d351d188e1d14
Alexey <waylander.exe@meta.ua>
2024-09-18 10:47:11 +0300

    modul 13

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import path, include
 from quotes.views import register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('add_author/', views.add_author, name='add_author'),
@@ -13,4 +14,14 @@ urlpatterns = [
     path('quotes/', include('quotes.urls')),
     path('accounts/register/', register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')), 
+]
+
+
+urlpatterns = [
+    path('add_author/', views.add_author, name='add_author'),
+    path('add_quote/', views.add_quote, name='add_quote'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- urls.py
+++ urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')),
 ]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('quotes.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
